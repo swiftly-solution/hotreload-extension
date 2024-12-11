c7d1d9bfafcc6b0e74aee19aea48ed67d6092f75
skuzzis <andreieurt@gmail.com>
2024-12-11 01:13:50 +0000

    update(src): FIle Changes Detector

--- src/entrypoint.cpp
+++ src/entrypoint.cpp
@@ -1,19 +1,38 @@
 #include "entrypoint.h"
+#include "utils.h"
+
+#include <thread>
+#include <fstream>
 
 //////////////////////////////////////////////////////////////
 /////////////////        Core Variables        //////////////
 ////////////////////////////////////////////////////////////
 
-BaseExtension g_Ext;
+SH_DECL_HOOK3_void(IServerGameDLL, GameFrame, SH_NOATTRIB, 0, bool, bool, bool);
+SH_DECL_HOOK1_void(IServerGameDLL, ServerHibernationUpdate, SH_NOATTRIB, 0, bool);
+
+HotReload g_Ext;
 CUtlVector<FuncHookBase *> g_vecHooks;
+
+IVEngineServer2* engine = nullptr;
+ISource2Server* server = nullptr;
+
 CREATE_GLOBALVARS();
 
 //////////////////////////////////////////////////////////////
 /////////////////          Core Class          //////////////
 ////////////////////////////////////////////////////////////
 
+void HotReloadError(std::string text)
+{
+    if (!g_SMAPI)
+        return;
+
+    g_SMAPI->ConPrintf("[Hot Reload] %s\n", text.c_str());
+}
+
 EXT_EXPOSE(g_Ext);
-bool BaseExtension::Load(std::string& error, SourceHook::ISourceHook *SHPtr, ISmmAPI* ismm, bool late)
+bool HotReload::Load(std::string& error, SourceHook::ISourceHook *SHPtr, ISmmAPI* ismm, bool late)
 {
     SAVE_GLOBALVARS();
     if(!InitializeHooks()) {
@@ -21,47 +40,149 @@ bool BaseExtension::Load(std::string& error, SourceHook::ISourceHook *SHPtr, ISm
         return false;
     }
 
-    ismm->ConPrint("Printing a text from extensions land!\n");
+    engine = (IVEngineServer *)ismm->VInterfaceMatch(ismm->GetEngineFactory(), INTERFACEVERSION_VENGINESERVER); 
+    if (!engine) { 
+        error = "Could not find interface: " INTERFACEVERSION_VENGINESERVER;
+        return false; 
+    }
+
+    server = (ISource2Server *)ismm->VInterfaceMatch(ismm->GetServerFactory(), INTERFACEVERSION_SERVERGAMEDLL, 0); 
+    if (!server) {
+        error = "Could not find interface: " INTERFACEVERSION_SERVERGAMEDLL;
+        return false;
+    }
+
+    // SH_ADD_HOOK_MEMFUNC(IServerGameDLL, GameFrame, server, this, &HTTPExtension::Hook_GameFrame, true);
+    // SH_ADD_HOOK_MEMFUNC(IServerGameDLL, ServerHibernationUpdate, server, this, &HTTPExtension::Hook_ServerHibernationUpdate, true);
+
+    LoadSettings();
+
     return true;
 }
 
-bool BaseExtension::Unload(std::string& error)
+bool HotReload::Unload(std::string& error)
 {
+    // SH_REMOVE_HOOK_MEMFUNC(IServerGameDLL, GameFrame, server, this, &HTTPExtension::Hook_GameFrame, true);
+    // SH_REMOVE_HOOK_MEMFUNC(IServerGameDLL, ServerHibernationUpdate, server, this, &HTTPExtension::Hook_ServerHibernationUpdate, true);
+
     UnloadHooks();
     return true;
 }
 
-void BaseExtension::AllExtensionsLoaded()
+void HotReload::LoadSettings()
+{
+    m_status = false;
+    m_plugins.clear();
+    m_interval = 5000;
+
+    std::ifstream ifs("addons/swiftly/configs/hotreload.json");
+    if(!ifs.is_open()) {
+        HotReloadError("Failed to open 'addons/swiftly/configs/hotreload.json'.");
+        return;
+    }
+
+    rapidjson::IStreamWrapper isw(ifs);
+    rapidjson::Document hotreloadFile;
+    hotreloadFile.ParseStream(isw);
+
+    if (hotreloadFile.HasParseError())
+        return HotReloadError(string_format("A parsing error has been detected.\nError (offset %u): %s\n", (unsigned)hotreloadFile.GetErrorOffset(), GetParseError_En(hotreloadFile.GetParseError())));
+
+    if (hotreloadFile.IsArray())
+        return HotReloadError("Hot reload configuration file cannot be an array.");
+
+    if(hotreloadFile["enabled"].IsBool())
+        SetStatus(hotreloadFile["enabled"].GetBool());
+
+    if(hotreloadFile["interval"].IsUint())
+        m_interval = hotreloadFile["interval"].GetUint();
+
+    if(hotreloadFile["plugins"].IsArray()) {
+        for(rapidjson::SizeType i = 0; i < hotreloadFile["plugins"].Size(); i++) {
+            if(hotreloadFile["plugins"][i].IsString()) {
+                m_plugins.push_back(hotreloadFile["plugins"][i].GetString());
+            }
+        }
+    }
+}
+
+void HotReload::SetStatus(bool status)
+{
+    m_status = status;
+}
+
+void HotReload::AllExtensionsLoaded()
 {
 
 }
 
-void BaseExtension::AllPluginsLoaded()
-{
+namespace fs = std::filesystem;
 
+void watchDirectory(const fs::path& path) {
+    std::unordered_map<std::string, fs::file_time_type> fileTimestamps;
+
+    for (const auto& entry : fs::recursive_directory_iterator(path)) {
+        if (fs::is_regular_file(entry)) {
+            fileTimestamps[entry.path().string()] = fs::last_write_time(entry);
+        }
+    }
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(g_Ext.GetInterval()));
+        if(g_Ext.GetStatus()) {
+            for (const auto& entry : fs::recursive_directory_iterator(path)) {
+                if (fs::is_regular_file(entry)) {
+                    auto path = entry.path().string();
+                    auto currentTimestamp = fs::last_write_time(entry);
+
+                    if (fileTimestamps.find(path) == fileTimestamps.end()) {
+
+                        fileTimestamps[path] = currentTimestamp;
+                    } else if (fileTimestamps[path] != currentTimestamp) {
+
+                        fileTimestamps[path] = currentTimestamp;
+                    }
+                }
+            }
+
+            for (auto it = fileTimestamps.begin(); it != fileTimestamps.end();) {
+                if (!fs::exists(it->first)) {
+
+                    it = fileTimestamps.erase(it);
+                } else {
+                    ++it;
+                }
+            }
+        }
+    }
 }
 
-bool BaseExtension::OnPluginLoad(std::string pluginName, void* pluginState, PluginKind_t kind, std::string& error)
+void HotReload::AllPluginsLoaded()
+{
+    std::thread(watchDirectory, "addons/swiftly/plugins").detach();
+}
+
+bool HotReload::OnPluginLoad(std::string pluginName, void* pluginState, PluginKind_t kind, std::string& error)
 {
     return true;
 }
 
-bool BaseExtension::OnPluginUnload(std::string pluginName, void* pluginState, PluginKind_t kind, std::string& error)
+bool HotReload::OnPluginUnload(std::string pluginName, void* pluginState, PluginKind_t kind, std::string& error)
 {
     return true;
 }
 
-const char* BaseExtension::GetAuthor()
+const char* HotReload::GetAuthor()
 {
     return "Swiftly Development Team";
 }
 
-const char* BaseExtension::GetName()
+const char* HotReload::GetName()
 {
     return "Base Extension";
 }
 
-const char* BaseExtension::GetVersion()
+const char* HotReload::GetVersion()
 {
 #ifndef VERSION
     return "Local";
@@ -70,7 +191,7 @@ const char* BaseExtension::GetVersion()
 #endif
 }
 
-const char* BaseExtension::GetWebsite()
+const char* HotReload::GetWebsite()
 {
     return "https://swiftlycs2.net/";
 }
--- src/entrypoint.h
+++ src/entrypoint.h
@@ -2,14 +2,29 @@
 #define _entrypoint_h
 
 #include <string>
+#include <vector>
 
 #include <swiftly-ext/core.h>
 #include <swiftly-ext/extension.h>
 #include <swiftly-ext/hooks/NativeHooks.h>
 
-class BaseExtension : public SwiftlyExt
+#include <rapidjson/document.h>
+#include <rapidjson/error/en.h>
+#include <rapidjson/istreamwrapper.h>
+
+#include <filesystem>
+#include <unordered_map>
+
+class HotReload : public SwiftlyExt
 {
+private:
+    bool m_status = false;
+    unsigned int m_interval = 5000;
+    std::vector<std::string> m_plugins;
+
 public:
+    std::unordered_map<std::string, std::filesystem::file_time_type> m_paths;
+
     bool Load(std::string& error, SourceHook::ISourceHook *SHPtr, ISmmAPI* ismm, bool late);
     bool Unload(std::string& error);
     
@@ -19,6 +34,15 @@ public:
     bool OnPluginLoad(std::string pluginName, void* pluginState, PluginKind_t kind, std::string& error);
     bool OnPluginUnload(std::string pluginName, void* pluginState, PluginKind_t kind, std::string& error);
 
+    void LoadSettings();
+    
+    void SetStatus(bool status);
+    bool GetStatus() { return m_status; }
+
+    std::vector<std::string> GetPlugins() { return m_plugins; }
+    
+    unsigned int GetInterval() { return m_interval; }
+
 public:
     const char* GetAuthor();
     const char* GetName();
@@ -26,7 +50,7 @@ public:
     const char* GetWebsite();
 };
 
-extern BaseExtension g_Ext;
+extern HotReload g_Ext;
 DECLARE_GLOBALVARS();
 
 #endif
